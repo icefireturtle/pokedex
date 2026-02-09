095413549995b17489dc5fab3e0719cafae3d8da
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-02-08 21:25:37 -0600

    created REPL function with infinite loop using bufio and scanner, clean the input and return the first word, updated main to welcome and call function from repl.go

--- repl.go
+++ repl.go
@@ -1,5 +1,26 @@
 package main
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
+func startREPL() {
+scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() 
+		clean := cleanInput(scanner.Text())
+		if len(clean) == 0 {
+			continue
+		}
+		firstWord := clean[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
 
 func cleanInput(text string) []string {
 	clean := strings.ToLower(strings.TrimSpace(text))
